7c763b1981aa21a0eba05ce6da3b8050929895a3
saulsilva99 <saulsilva99@gmail.com>
2018-11-05 16:33:21 +0000

    exercicio 31 e 33)

---
+++ Semana6/src/exercicio32/testReadNumbers.java
@@ -0,0 +1,11 @@
+package exercicio32;
+
+public class testReadNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReadNumbers r = new ReadNumbers();
+		r.number();
+	}
+
+}
