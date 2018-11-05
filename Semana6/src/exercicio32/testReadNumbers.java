2e896086ca7506b23be2409d72fa54508dc7129e
saulsilva99 <saulsilva99@gmail.com>
2018-11-05 23:19:41 +0000

    2 files added again

---
+++ Semana6/src/exercicio32/testReadNumbers.java
@@ -0,0 +1,12 @@
+package exercicio32;
+
+public class testReadNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReadNumbers r = new ReadNumbers();
+		r.number();
+		system.out.print("ola");
+	}
+
+}
